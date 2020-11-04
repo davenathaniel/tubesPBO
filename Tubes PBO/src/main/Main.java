9b6f9d7166e5e8ba60a45790951ea197bc668d50
maria <maria@LAPTOP-EMVY>
2020-11-04 11:20:25 +0700

    create view dasar untuk login register

--- Tubes PBO/src/main/Main.java
+++ Tubes PBO/src/main/Main.java
@@ -15,11 +15,12 @@ package main;
  * 
  */
 import controller.Controller;
+import view.MenuUtama;
 
 public class Main {
     static Controller controller = new Controller();
 
     public static void main(String[] args) {
-        
+        new MenuUtama();
     }
 }
