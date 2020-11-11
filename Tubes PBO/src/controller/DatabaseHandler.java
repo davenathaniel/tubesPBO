bb88a273b1b06a12debe46d363cb812f5c87f9d3
Dave <LENOVO G4-45@Dread>
2020-11-11 07:09:16 +0700

    update connection to database

--- Tubes PBO/src/controller/DatabaseHandler.java
+++ Tubes PBO/src/controller/DatabaseHandler.java
@@ -17,7 +17,7 @@ public class DatabaseHandler {
     public Connection con;
     private String driver = "com.mysql.cj.jdbc.Driver";
     private String url = "jdbc:mysql://localhost/db_tubes_pbo";
-   // private String url = "jdbc:mysql://localhost/db_tubes_pbo?serverTimezone=" + TimeZone.getDefault().getID();
+    // private String url = "jdbc:mysql://localhost/db_tubes_pbo?serverTimezone=" + TimeZone.getDefault().getID();
     private String username = "root";
     private String password = "";
 
@@ -49,7 +49,7 @@ public class DatabaseHandler {
         try {
             con = logOn();
         } catch (Exception ex) {
-            System.out.println("Error occured when connecting to database");
+            System.out.println("Error Occured when connecting to database");
         }
     }
 
@@ -57,7 +57,7 @@ public class DatabaseHandler {
         try {
             logOff();
         } catch (Exception ex) {
-            System.out.println("Error occured when connecting to database");
+            System.out.println("Error Occured when connecting to database");
         }
     }
 }
