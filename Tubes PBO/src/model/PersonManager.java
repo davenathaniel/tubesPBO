66418c60e0ac97a92399c8dc9de8e3331f0fc5ab
Dave <LENOVO G4-45@Dread>
2020-11-11 07:39:43 +0700

    create class person manager

---
+++ Tubes PBO/src/model/PersonManager.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Dave
+ */
+public class PersonManager {
+
+    private static PersonManager instance;
+    private Person person;
+
+    public static PersonManager getInstance() {
+        if (instance == null) {
+            instance = new PersonManager();
+        }
+        return instance;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+}
